7511e8bde96b1a5ee09b0f1306c03896130c3729
LeoKuD <then7w@gmail.com>
2022-03-10 16:34:05 +0300

    refactor(createmembermodal): add map

--- src/components/Common/Modal/CreateMemberModal/CreateMemberModal.jsx
+++ src/components/Common/Modal/CreateMemberModal/CreateMemberModal.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { initialStateCreatMember } from '../../../../shared/store';
-import { BUTTONS_TYPES, BUTTONS_NAMES, DIRECTIONS_KEYS, SEX_KEYS, USER_ROLES } from '../../../../shared/constants';
+import { BUTTONS_TYPES, BUTTONS_NAMES, USER_FIELDS_KEYS } from '../../../../shared/constants';
 import { Button } from '../../../Buttons/Button/Button';
 import { ModalRow } from '../ModalRow/ModalRow';
 import style from './CreateMemberModal.module.css';
@@ -28,76 +28,25 @@ export class CreateMemberModal extends React.Component {
   };
 
   render() {
-    const {
-      name,
-      lastName,
-      email,
-      direction,
-      sex,
-      role,
-      password,
-      confirmPassword,
-      birthDate,
-      address,
-      phone,
-      skype,
-      startDate,
-      education,
-      universityAverageAcore,
-      mathScore,
-    } = this.state;
-
     return (
       <form onSubmit={this.handleSubmit} className={style.wrapper}>
-        <div className={style.section}>
-          <ModalRow onChange={this.handleChange} value={name} name='name' title='Name:' />
-          <ModalRow onChange={this.handleChange} value={lastName} name='lastName' title='Last name:' />
-          <ModalRow onChange={this.handleChange} value={email} name='email' title='Email:' />
-          <ModalRow
-            onChange={this.handleChange}
-            value={direction}
-            type='select'
-            name='direction'
-            title='Direction:'
-            options={DIRECTIONS_KEYS}
-          />
-          <ModalRow onChange={this.handleChange} value={sex} type='select' name='sex' title='Sex:' options={SEX_KEYS} />
-          <ModalRow
-            onChange={this.handleChange}
-            value={role}
-            type='select'
-            name='role'
-            title='Role:'
-            options={USER_ROLES}
-          />
-          <ModalRow onChange={this.handleChange} value={password} name='password' title='Password:' />
-          <ModalRow
-            onChange={this.handleChange}
-            value={confirmPassword}
-            name='confirmPassword'
-            title='Confirm password:'
-          />
-        </div>
-        <div className={style.section}>
-          <ModalRow
-            onChange={this.handleChange}
-            value={birthDate}
-            type='date'
-            name='birthDate'
-            title='Date of Birth:'
-          />
-          <ModalRow onChange={this.handleChange} value={address} name='address' title='Address:' />
-          <ModalRow onChange={this.handleChange} value={phone} name='phone' title='Mobile phone:' />
-          <ModalRow onChange={this.handleChange} value={skype} name='skype' title='Skype:' />
-          <ModalRow onChange={this.handleChange} value={startDate} type='date' name='startDate' title='Start date:' />
-          <ModalRow onChange={this.handleChange} value={education} name='education' title='Education:' />
-          <ModalRow
-            onChange={this.handleChange}
-            value={universityAverageAcore}
-            name='universityAverageAcore'
-            title='University average score:'
-          />
-          <ModalRow onChange={this.handleChange} value={mathScore} name='mathScore' title='Math score:' />
+        <div className={style.section__fields}>
+          {USER_FIELDS_KEYS.map((item) => {
+            const { name, title, type, options } = item;
+            const { ...state } = this.state;
+
+            return (
+              <ModalRow
+                key={item.name}
+                onChange={this.handleChange}
+                value={state[name]}
+                name={name}
+                options={options}
+                type={type}
+                title={title}
+              />
+            );
+          })}
         </div>
         <div className={style.section__buttons}>
           <Button onClick={this.handleCreateUser} />
--- src/shared/constants.js
+++ src/shared/constants.js
@@ -99,3 +99,21 @@ export const LINKPATH_KEYS = {
 export const MODALTITLE_KEYS = {
   createMember: 'Create member',
 };
+
+export const USER_FIELDS_KEYS = [
+  { name: 'name', title: 'Name:', type: 'text' },
+  { name: 'lastName', title: 'Last name:', type: 'text' },
+  { name: 'email', title: 'Email:', type: 'text' },
+  { name: 'direction', title: 'Direction:', type: 'select', options: DIRECTIONS_KEYS },
+  { name: 'sex', title: 'Sex:', type: 'select', options: SEX_KEYS },
+  { name: 'role', title: 'Role:', type: 'select', options: USER_ROLES },
+  { name: 'password', title: 'Password:', type: 'text' },
+  { name: 'confirmPassword', title: 'Confirm password:', type: 'text' },
+  { name: 'birthDate', title: 'Date of Birth:', type: 'date' },
+  { name: 'address', title: 'Mobile phone:', type: 'text' },
+  { name: 'skype', title: 'Skype:', type: 'text' },
+  { name: 'startDate', title: 'Start date:', type: 'date' },
+  { name: 'education', title: 'Education:', type: 'text' },
+  { name: 'universityAverageAcore', title: 'University average score:', type: 'text' },
+  { name: 'mathScore', title: 'Math score:', type: 'text' },
+];
