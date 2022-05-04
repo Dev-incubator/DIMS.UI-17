3d83724c1a7b503f4f32ddb22c5d76a2a19ca79a
LeoKuD <then7w@gmail.com>
2022-05-04 20:06:51 +0300

    feat(header, settingspage): add useContext()

--- src/components/Common/Header/Header.jsx
+++ src/components/Common/Header/Header.jsx
@@ -1,3 +1,4 @@
+import { useContext } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import style from './Header.module.css';
 import logo from '../../../assets/img/logo.png';
@@ -9,51 +10,40 @@ import { ReactComponent as SettingsLogo } from '../../../assets/img/settings.svg
 import { SettingsPge } from '../SettingsPage/SetingsPage';
 
 export function Header() {
-  return (
-    <SettingsContext.Consumer>
-      {({ toggleSettings, isShowSettings }) => {
-        return (
-          <AuthContext.Consumer>
-            {({ isAuth, role }) => {
-              const links =
-                role === USER_ROLES.user
-                  ? LINKS_HEADER_MENU.filter((item) => item.access === USER_ROLES.user)
-                  : LINKS_HEADER_MENU;
+  const { toggleSettings, isShowSettings } = useContext(SettingsContext);
+  const { isAuth, role } = useContext(AuthContext);
 
-              return (
-                <header className={style.header}>
-                  <div className={style.contentWrapper}>
-                    <div className={style.leftContent}>
-                      <Link to='/'>
-                        <img className={style.logo} src={logo} alt='logo' />
-                      </Link>
-                      <nav>
-                        <ul className={style.headerNav}>
-                          {links.map((item) => (
-                            <NavLink
-                              key={item.name}
-                              to={item.to}
-                              className={style[item.className]}
-                              activeClassName={style[item.activeClassName]}
-                            >
-                              {item.name}
-                            </NavLink>
-                          ))}
-                        </ul>
-                      </nav>
-                    </div>
-                    <div className={style.rightContent}>
-                      <SettingsLogo className={style.settings} onClick={toggleSettings} />
-                      <Link to='/login'>{isAuth ? <Logout /> : `Login`}</Link>
-                      {isShowSettings ? <SettingsPge /> : null}
-                    </div>
-                  </div>
-                </header>
-              );
-            }}
-          </AuthContext.Consumer>
-        );
-      }}
-    </SettingsContext.Consumer>
+  const links =
+    role === USER_ROLES.user ? LINKS_HEADER_MENU.filter((item) => item.access === USER_ROLES.user) : LINKS_HEADER_MENU;
+
+  return (
+    <header className={style.header}>
+      <div className={style.contentWrapper}>
+        <div className={style.leftContent}>
+          <Link to='/'>
+            <img className={style.logo} src={logo} alt='logo' />
+          </Link>
+          <nav>
+            <ul className={style.headerNav}>
+              {links.map((item) => (
+                <NavLink
+                  key={item.name}
+                  to={item.to}
+                  className={style[item.className]}
+                  activeClassName={style[item.activeClassName]}
+                >
+                  {item.name}
+                </NavLink>
+              ))}
+            </ul>
+          </nav>
+        </div>
+        <div className={style.rightContent}>
+          <SettingsLogo className={style.settings} onClick={toggleSettings} />
+          <Link to='/login'>{isAuth ? <Logout /> : `Login`}</Link>
+          {isShowSettings ? <SettingsPge /> : null}
+        </div>
+      </div>
+    </header>
   );
 }
